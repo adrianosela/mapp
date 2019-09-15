74c72cbe28903bdecf7309153e34f93a15f6f557
Adriano <adriano.selaviles@gmail.com>
2019-09-15 13:14:51 -0700

    add simple express app

---
+++ api/index.js
@@ -0,0 +1,13 @@
+// Load express module
+var express = require('express')
+var app = express()
+
+// Define endpoints
+app.get('/healthcheck', function (req, res) {
+  res.send('server is up and running!')
+})
+
+// Serve HTTP
+app.listen(8080, function () {
+  console.log('app listening on port 8080!')
+})
