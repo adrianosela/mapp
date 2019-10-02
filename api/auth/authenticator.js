02fbd68341b163ef384a90b64c79df815ff2eef8
Adriano <adriano.selaviles@gmail.com>
2019-10-02 12:36:38 -0700

    document auth mechanism with google

--- api/auth/authenticator.js
+++ api/auth/authenticator.js
@@ -1,16 +1,43 @@
 const { OAuth2Client } = require('google-auth-library');
 
+/**
+ * the scopes listed below dictate what permissions
+ * Google will ask the user for consent for
+ */
 const defaultScope = [
   'https://www.googleapis.com/auth/userinfo.profile',
   'https://www.googleapis.com/auth/userinfo.email',
 ];
 
+// Url to get user profile with an authenticated client (using user's access token)
+const fetchProfileUrl = 'https://people.googleapis.com/v1/people/me?personFields=names,emailAddresses';
+
+/**
+ * Authenticator is a Google authenticator class
+ * responsible for retrieving, and verifying user
+ * auth tokens from Google.
+ */
 class Authenticator {
-  constructor() {}
- 
+  constructor() { /* NOP */ }
+    
+  /**
+   * Sets Google admin API credentials and generates
+   * the URL through which Mapp users will authenticate
+   * against Google.
+   *
+   * @param {string} cid - Google Admin API ClientID
+   * @param {string} csecret - Google Admin API ClientSecret
+   * @param {string} redirUrl - The callback URL where authenticated
+   *                            users come back to with a token (where 
+   *                            Google redirects users to after they
+   *                            have successfully authenticated
+   */
   initialize(cid, csecret, redirUrl) {
-    this.oauth2client = new OAuth2Client(cid, csecret, redirUrl);
-    this.url = this.oauth2client.generateAuthUrl({
+    this.clientId = cid;
+    this.clientSecret = csecret;
+    this.redirectUrl = redirUrl;
+    const client = new OAuth2Client(cid, csecret, redirUrl);
+    this.url = client.generateAuthUrl({
       access_type: 'offline',
       prompt: 'consent',
       scope: defaultScope
@@ -18,16 +45,34 @@ class Authenticator {
     console.log('[info] successfully initialized google auth client');
   }
   
-  getUrl() {
+  /**
+   * Returns the URL where users must consent to using Mapp
+   */
+  getGoogleAuthUrl() {
     return this.url;
   }
 
-  async getGoogleUser(code) {
-    const data = await this.oauth2client.getToken(code)
-    this.oauth2client.setCredentials(data.tokens);
-    const url = 'https://people.googleapis.com/v1/people/me?personFields=names,emailAddresses';
-    const res = await this.oauth2client.request({url});
-    
+  /**
+   * Returns the id, access, and refresh tokens for the user
+   *
+   * @param {string} code - 'Authorization successful code' from Google
+   *                        with which we can fetch the user's auth tokens
+   */
+  async getGoogleAuthTokens(code) {
+    const client = new OAuth2Client(this.clientId, this.clientSecret, this.redirectUrl);
+    const data = await client.getToken(code);
+    return data.tokens;
+  }
+
+  /**
+   * Returns the user profile (with the default scopes above the Authenticator class)
+   *
+   * @param {string} accessToken - the user's access token to Google APIs
+   */
+  async getGoogleUserProfile(accessToken) {
+    const client = new OAuth2Client(this.clientId, this.clientSecret, this.redirectUrl);
+    client.setCredentials({access_token: accessToken});
+    const res = await client.request({url: fetchProfileUrl});
     return res.data;
   }
 }
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -2,17 +2,25 @@ const router = require('express').Router();
 const authenticator = require('../auth/authenticator');
 
 router.get('/login', function(req, resp) {
-  resp.send(authenticator.getURL());
+  resp.send(authenticator.getGoogleAuthUrl());
 });
 
-router.get('/googlecallback', async function (req, resp) {
-  await authenticator.getGoogleUser(req.query.code).then((u) => {
-    console.log(u);
-    resp.end('authentication successful, return to mapp');
+router.get('/googlecallback', async function(req, resp) {
+  await authenticator.getGoogleAuthTokens(req.query.code).then(async function(tokens){
+  	console.log(tokens);
+  	await authenticator.getGoogleUserProfile(tokens.access_token).then((profile) => {
+  		console.log(profile);
+  		resp.end('authentication successful, return to mapp');
+  	}).catch((e) => {
+  		console.log(e);
+  		resp.status(401).send('could not get user profile');
+  	});
   }).catch((e) => {
     console.log(e);
     resp.status(401).send('failed to authenticate :(');
   });
 });
+
+
 
 module.exports = router;
