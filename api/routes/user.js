3b9e00852a2b6203c3f4ca1765516ed3a517de4d
Adriano <adriano.selaviles@gmail.com>
2019-10-20 16:52:13 -0700

    user registration endpoint and splitting of auth and app users

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const bcrypt = require('bcrypt');
 
 // import User & UserSettings schemas
 let User = require('../models/user');
@@ -7,49 +8,50 @@ let UserSettings = require('../models/userSettings');
 // register a new user into mapp
 router.post('/register', async function(req, resp) {
     // check fields are set
-    if (!req.body.email) { resp.status(400, 'no email provided'); }
-    if (!req.body.password) { resp.status(400, 'no password provided'); }
-    if (!req.body.name) { resp.status(400, 'no preffered name provided'); }
+    if (!req.body.email) { resp.status(400).send('no email provided'); }
+    if (!req.body.password) { resp.status(400).send('no password provided'); }
+    if (!req.body.name) { resp.status(400).send('no preffered name provided'); }
 
-    // check email is unused
-    let user = await UserSettings.find({ email: req.body.email });
-    if (user) { resp.status(400, 'user with that email exists'); }
+    // check email is not used
+    UserSettings.findOne( {email: req.body.email}, function (err, user) {
+		if (user) { resp.status(400).send('user with that email already exists'); }
+    });
+
+    // hash password and store
+    const hashed = await bcrypt.hash(req.body.password, 10);
 
     // populate new user settings schema
     let newUserSettings = new UserSettings({
-	email: req.body.email,
-	hash: hashed
+		email: req.body.email,
+		hash: hashed
     });
 
     // save new user settings
     let savedUserSettings;
     try {
-	savedUserSettings = await newUserSettings.save();
+		savedUserSettings = await newUserSettings.save();
     } catch (e) {
-	console.log(e);
-	resp.status(500, 'could not save new user settings');
+		console.log(e);
+		resp.status(500).send('could not save new user settings');
     }
 
     // populate new user schema
     let newUser = new User({
-	_id: savedUserSettings._id, // same BSON id as user settings
-	name: req.body.name
+		_id: savedUserSettings._id, // same BSON id as user settings
+		name: req.body.name
     });
 
     // save new user
     let savedUser;
     try {
-	savedUser = await newUser.save();
+		savedUser = await newUser.save();
     } catch (e) {
-	console.log(e);
-	resp.status(500, 'could not save new user');
+		console.log(e);
+		resp.status(500).send('could not save new user');
     }
 
     // return saved user
     resp.json(savedUser);
 });
-
-// trade basic credentials for a signed JWT
-router.get('/login'
 
 module.exports = router;
