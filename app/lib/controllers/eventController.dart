f90f86678f2f3453a084c4fd23c864328c6109c0
chrisneuf <neufeld.christian1@gmail.com>
2019-10-20 17:10:31 -0700

    code cleanup

--- app/lib/controllers/eventController.dart
+++ app/lib/controllers/eventController.dart
@@ -40,13 +40,13 @@ class EventController {
 
   Future<String> createEvent(String url, body) async {
     return http.post(url, headers: {"Content-Type": "application/json"}, body: jsonEncode(body)).then((http.Response response) {
+
       final int statusCode = response.statusCode;
-      print(jsonEncode(body));
+
       if (statusCode < 200 || statusCode > 400 || json == null) {
-        print(statusCode);
-        print(json);
         throw new Exception("Error while fetching data");
       }
+
       Map<String, dynamic> jsonResponse = json.decode(response.body);
       return jsonResponse["data"]["eventId"];
     });
