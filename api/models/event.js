f6d99edb96ee0b2cfd8d385bf8256c9375bebafa
israeltq <israeltrujilloq96@gmail.com>
2019-10-31 00:13:26 -0700

    Added eslint package, major cleanup

--- api/models/event.js
+++ api/models/event.js
@@ -1,36 +1,36 @@
 let mongoose = require("mongoose");
 
 let eventSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  description: { type: String, required: false, default: "" },
+    name: { type: String, required: true },
+    description: { type: String, required: false, default: "" },
 
-  // time/place
-  location: {
-    type: { type: String, required: true },
-    coordinates: []
-  },
-  startTime: { type: Number, required: true },
-  endTime: { type: Number, required: true },
+    // time/place
+    location: {
+        type: { type: String, required: true },
+        coordinates: []
+    },
+    startTime: { type: Number, required: true },
+    endTime: { type: Number, required: true },
 
-  // people
-  creator: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true
-  },
-  followers: [
-    { type: mongoose.Schema.Types.ObjectId, ref: "User", required: false }
-  ],
-  invited: [
-    { type: mongoose.Schema.Types.ObjectId, ref: "User", required: false }
-  ],
+    // people
+    creator: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    followers: [
+        { type: mongoose.Schema.Types.ObjectId, ref: "User", required: false }
+    ],
+    invited: [
+        { type: mongoose.Schema.Types.ObjectId, ref: "User", required: false }
+    ],
 
-  // metadata
-  categories: { type: [String], required: false, default: [] },
-  public: { type: Boolean, required: false, default: true }
+    // metadata
+    categories: { type: [String], required: false, default: [] },
+    public: { type: Boolean, required: false, default: true }
 });
 
 // set geospatial indexing
 eventSchema.index({ location: "2dsphere" });
 
-let Event = (module.exports = mongoose.model("Event", eventSchema));
+module.exports = mongoose.model("Event", eventSchema);
