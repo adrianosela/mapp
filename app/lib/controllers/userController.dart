c96dc069bafb3cba2d8686eab162df06239a96fb
chrisneuf <neufeld.christian1@gmail.com>
2019-11-14 20:31:59 -0800

    user search

--- app/lib/controllers/userController.dart
+++ app/lib/controllers/userController.dart
@@ -3,25 +3,22 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 
-
 class UserController {
-
   ///Get a map of users the current user is following => map(userid, username)
   static Future<Map<String, String>> getUserFollowing(token) async {
-
-    var uri = Uri.https(
-      "mapp-254321.appspot.com",
-      "/user/following"
-    );
+    var uri = Uri.https("mapp-254321.appspot.com", "/user/following");
 
     Map<String, String> following = new Map<String, String>();
 
-    final response = await http.get(uri, headers: {"Content-Type": "application/json", "authorization" : "Bearer $token"});
+    final response = await http.get(uri, headers: {
+      "Content-Type": "application/json",
+      "authorization": "Bearer $token"
+    });
 
     if (response.statusCode == 200) {
       var userContainer = json.decode(response.body);
-      if(userContainer != null) {
-        for(var instance in userContainer) {
+      if (userContainer != null) {
+        for (var instance in userContainer) {
           following[instance["id"].toString()] = instance["name"].toString();
         }
       }
@@ -32,10 +29,56 @@ class UserController {
     return following;
   }
 
+  static Future<Map<String, String>> searchUsers(token, String search) async {
+    Map<String, String> query = {
+      'username': search,
+    };
+
+    var uri = Uri.https(
+      "mapp-254321.appspot.com",
+      "/user/search",
+      query,
+    );
+
+    Map<String, String> following = new Map<String, String>();
+
+    final response = await http.get(uri, headers: {
+      "Content-Type": "application/json",
+      "authorization": "Bearer $token"
+    });
+
+    print(response.body);
+
+    if (response.statusCode == 200) {
+      var userContainer = json.decode(response.body);
+      if (userContainer != null) {
+        for (var instance in userContainer) {
+          following[instance["id"].toString()] = instance["name"].toString();
+        }
+      }
+    } else {
+      // If that response was not OK, throw an error.
+      throw Exception('Failed to fetch data');
+    }
+    return following;
+  }
 
   ///TODO finish this send a "follow user post"
-  static Future<String> followUser(String url, body, token) async {
-    return http.post(url, headers: {"Content-Type": "application/json", "authorization" : "Bearer $token"}, body: jsonEncode(body)).then((http.Response response) {
+  static Future<String> followUser(token, String userId) async {
+    var uri = Uri.https(
+      "mapp-254321.appspot.com",
+      "/user/follow",
+    );
+
+    var body = {"userToFollowId": userId};
+    return http
+        .post(uri,
+            headers: {
+              "Content-Type": "application/json",
+              "authorization": "Bearer $token"
+            },
+            body: jsonEncode(body))
+        .then((http.Response response) {
       final int statusCode = response.statusCode;
       print(jsonEncode(body));
       if (statusCode < 200 || statusCode > 400 || json == null) {
@@ -48,13 +91,9 @@ class UserController {
     });
   }
 
-
   ///Get events user clicked "going to"
   static Future<Map<String, String>> getCreatedEvents(String id) async {
-
-    Map<String, String> query = {
-      'id' : id
-    };
+    Map<String, String> query = {'id': id};
 
     var uri = Uri.https(
       "mapp-254321.appspot.com",
@@ -64,14 +103,16 @@ class UserController {
 
     Map<String, String> result = new Map<String, String>();
 
-    final response = await http.get(uri, headers: {"Content-Type": "application/json", "authorization" : "Bearer $id"});
-
+    final response = await http.get(uri, headers: {
+      "Content-Type": "application/json",
+      "authorization": "Bearer $id"
+    });
 
     if (response.statusCode == 200) {
       var userContainer = json.decode(response.body);
 
-      if(userContainer != null) {
-        for(var instance in userContainer) {
+      if (userContainer != null) {
+        for (var instance in userContainer) {
           result[instance["id"].toString()] = instance["name"].toString();
         }
       }
@@ -82,13 +123,9 @@ class UserController {
     return result;
   }
 
-
   ///Get events user clicked "going to"
   static Future<Map<String, String>> getSubscribedEvents(String id) async {
-
-    Map<String, String> query = {
-      'id' : id
-    };
+    Map<String, String> query = {'id': id};
 
     var uri = Uri.https(
       "mapp-254321.appspot.com",
@@ -98,14 +135,16 @@ class UserController {
 
     Map<String, String> result = new Map<String, String>();
 
-    final response = await http.get(uri, headers: {"Content-Type": "application/json", "authorization" : "Bearer $id"});
-
+    final response = await http.get(uri, headers: {
+      "Content-Type": "application/json",
+      "authorization": "Bearer $id"
+    });
 
     if (response.statusCode == 200) {
       var userContainer = json.decode(response.body);
 
-      if(userContainer != null) {
-        for(var instance in userContainer) {
+      if (userContainer != null) {
+        for (var instance in userContainer) {
           result[instance["id"].toString()] = instance["name"].toString();
         }
       }
@@ -116,13 +155,9 @@ class UserController {
     return result;
   }
 
-
   ///Get events user is invited to
   static Future<Map<String, String>> getPendingEvents(String id) async {
-
-    Map<String, String> query = {
-      'id' : id
-    };
+    Map<String, String> query = {'id': id};
 
     var uri = Uri.https(
       "mapp-254321.appspot.com",
@@ -132,12 +167,15 @@ class UserController {
 
     Map<String, String> result = new Map<String, String>();
 
-    final response = await http.get(uri, headers: {"Content-Type": "application/json", "authorization" : "Bearer $id"});
+    final response = await http.get(uri, headers: {
+      "Content-Type": "application/json",
+      "authorization": "Bearer $id"
+    });
 
     if (response.statusCode == 200) {
       var userContainer = json.decode(response.body);
-      if(userContainer != null) {
-        for(var instance in userContainer) {
+      if (userContainer != null) {
+        for (var instance in userContainer) {
           result[instance["id"].toString()] = instance["name"].toString();
         }
       }
@@ -148,16 +186,21 @@ class UserController {
     return result;
   }
 
-
   ///Respond "going" to event invite
   static Future<String> postSubscribe(String token, body) async {
-
     var uri = Uri.https(
       "mapp-254321.appspot.com",
       "/user/subscribe",
     );
 
-    return http.post(uri, headers: {"Content-Type": "application/json", "authorization" : "Bearer $token"}, body: jsonEncode(body)).then((http.Response response) {
+    return http
+        .post(uri,
+            headers: {
+              "Content-Type": "application/json",
+              "authorization": "Bearer $token"
+            },
+            body: jsonEncode(body))
+        .then((http.Response response) {
       final int statusCode = response.statusCode;
       print(jsonEncode(body));
       if (statusCode < 200 || statusCode > 400 || json == null) {
@@ -170,16 +213,21 @@ class UserController {
     });
   }
 
-
   ///Respond "not going" to event invite
   static Future<String> postNotGoing(String token, body) async {
-
     var uri = Uri.https(
       "mapp-254321.appspot.com",
       "/user/declineInvite",
     );
 
-    return http.post(uri, headers: {"Content-Type": "application/json", "authorization" : "Bearer $token"}, body: jsonEncode(body)).then((http.Response response) {
+    return http
+        .post(uri,
+            headers: {
+              "Content-Type": "application/json",
+              "authorization": "Bearer $token"
+            },
+            body: jsonEncode(body))
+        .then((http.Response response) {
       final int statusCode = response.statusCode;
       print(jsonEncode(body));
       if (statusCode < 200 || statusCode > 400 || json == null) {
@@ -192,16 +240,21 @@ class UserController {
     });
   }
 
-
   ///Unsubscribe from an event
   static Future<String> postUnsubscribe(String token, body) async {
-
     var uri = Uri.https(
       "mapp-254321.appspot.com",
       "/user/unsubscribe",
     );
 
-    return http.post(uri, headers: {"Content-Type": "application/json", "authorization" : "Bearer $token"}, body: jsonEncode(body)).then((http.Response response) {
+    return http
+        .post(uri,
+            headers: {
+              "Content-Type": "application/json",
+              "authorization": "Bearer $token"
+            },
+            body: jsonEncode(body))
+        .then((http.Response response) {
       final int statusCode = response.statusCode;
       print(jsonEncode(body));
       if (statusCode < 200 || statusCode > 400 || json == null) {
