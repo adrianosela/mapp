ecefe41264e9b682e729d6cd99c4d9c3966eea71
Adriano <adriano.selaviles@gmail.com>
2019-11-16 14:51:48 -0800

    finish up auth tests

--- api/__tests__/integration/auth.test.js
+++ api/__tests__/integration/auth.test.js
@@ -4,95 +4,71 @@ const app = require("../../server");
 const User = require("../../models/user");
 const request = supertest(app);
 
-describe("Test Auth Endpoints", function() {
-
+describe("Test Authentication Handlers", function() {
+    // connect to mock mongo before testing
     beforeAll(async function() {
-        await mongoose.connect(process.env.MONGO_URL, { 
+        await mongoose.connect(process.env.MONGO_URL, {
             useNewUrlParser: true,
-            useUnifiedTopology: true 
+            useUnifiedTopology: true
         });
     });
-
+    // wipe mock mongo after testing
     afterAll(async function() {
-        await mongoose.connection.close();
-    });
-
-    beforeEach(async function() {
-        const reqBody = {
-            email: "testing@gmail.com",
-            password: "testing123",
-            name: "Test Dummy"
-        };
-
-        await request.post("/register").send(reqBody);
-    });
-
-    afterEach(async function() {
         const collections = Object.keys(mongoose.connection.collections);
         for (const collectionName of collections) {
             const collection = mongoose.connection.collections[collectionName];
             await collection.deleteMany();
         }
+        await mongoose.connection.close();
     });
 
-    describe("Test Register User", function() {
+    describe("Positive: Register User", function() {
         it("Should register new user to database", async function() {
             const reqBody = {
                 email: "test@gmail.com",
                 password: "testing123",
                 name: "Auth Test"
             };
-            
             const res = await request.post("/register").send(reqBody);
             expect(res.status).toBe(200);
 
-            const newUser = await User.findOne({
-                name: "Auth Test"
-            });
+            const newUser = await User.findOne({name: "Auth Test"});
             expect(newUser._id.toString()).toEqual(res.body._id);
             expect(newUser.name).toEqual(res.body.name);
         });
+    });
 
-        it("Should return 400 (Bad Request) when user already exists", async function() {
+    describe("Positive: Login User", function() {
+        it("Should login user", async function() {
             const reqBody = {
-                email: "testing@gmail.com",
+                email: "test@gmail.com",
                 password: "testing123",
-                name: "Test Dummy"
+                name: "Auth Test"
             };
-
-            const res = await request.post("/register").send(reqBody);
-            expect(res.status).toBe(400);
+            const res = await request.post("/login").send(reqBody);
+            expect(res.status).toBe(200);
         });
     });
 
-    describe("Test Login User", function() {
-        it("Should login an existing user", async function() {
+    describe("Negative: Login User With Bad Password", function() {
+        it("Should not login user with incorrect password", async function() {
             const reqBody = {
-                email: "testing@gmail.com",
-                password: "testing123"
+                email: "test@gmail.com",
+                password: "badpassword",
+                name: "Auth Test"
             };
-
-            const res = await request.post("/login").send(reqBody);
-            expect(res.status).toBe(200);
-            expect(res.body.token).toBeTruthy();
-        });
-
-        it("Should return 401 (forbidden) with incorrect password", async function() {
-            const reqBody = {
-                email: "testing@gmail.com",
-                password: "incorrect"
-            };
-
             const res = await request.post("/login").send(reqBody);
             expect(res.status).toBe(401);
         });
+    });
 
-        it("Should return 401 (forbidden) with non-registered user", async function() {
+    describe("Negative: Login User Not in DB", function() {
+        it("Should not login user with bad password", async function() {
             const reqBody = {
                 email: "incorrect@gmail.com",
                 password: "incorrect"
-            };
 
+            };
             const res = await request.post("/login").send(reqBody);
             expect(res.status).toBe(401);
         });
