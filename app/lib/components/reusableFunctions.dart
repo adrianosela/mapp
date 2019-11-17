f5696b8bd0ef12f7b796b3c934a9b0b7b1bca5fb
Oleksandra <oleksandra_nikulina@hotmail.com>
2019-11-17 15:44:03 -0800

    fixed event details popup

--- app/lib/components/reusableFunctions.dart
+++ app/lib/components/reusableFunctions.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:intl/intl.dart';
+
 import 'package:app/components/router.dart';
 import 'package:app/components/reusableStlyes.dart';
 
@@ -8,6 +10,7 @@ import 'package:app/controllers/eventController.dart';
 
 import 'package:app/models/userModel.dart';
 import 'package:app/models/fcmToken.dart';
+import 'package:app/models/eventModel.dart';
 
 import 'package:app/screens/map.dart';
 
@@ -42,8 +45,7 @@ class ReusableFunctions{
         style: ReusableStyles.listItem(),
       ),
       onTap: () async {
-        var response = await EventController.getEvent(userToken, id.toString());
-        //TODO add styling
+        Event event = await EventController.getEventObject(userToken, id);
         showDialog(
             context: context,
             builder: (BuildContext context)
@@ -55,7 +57,7 @@ class ReusableFunctions{
                     children: <Widget>[
                       Padding(
                           padding: EdgeInsets.all(4.0),
-                          child: Text(response["name"],
+                          child: Text(event.name,
                               style: TextStyle(
                                   fontWeight: FontWeight.bold,
                                   fontSize: 25,
@@ -66,7 +68,7 @@ class ReusableFunctions{
                       Padding(
                           padding: EdgeInsets.all(3.0),
                           child: Text(
-                              "Description: " + response["description"],
+                              "Description: " + event.description,
                               style: TextStyle(
                                   fontWeight: FontWeight.bold,
                                   fontSize: 17.0
@@ -75,22 +77,7 @@ class ReusableFunctions{
                       ),
                       Padding(
                           padding: EdgeInsets.all(3.0),
-                          //TODO fix how this is displayed
-                          child: Text("Start Time: " +
-                              DateTime.fromMillisecondsSinceEpoch(
-                                  int.parse(response["startTime"]) * 1000)
-                                  .toString())
-                      ),
-                      Padding(
-                          padding: EdgeInsets.all(3.0),
-                          //TODO fix how this is displayed
-                          child: Text("End Time: " +
-                              DateTime.fromMillisecondsSinceEpoch(
-                                  int.parse(response["endTime"]) * 1000).toString())
-                      ),
-                      Padding(
-                          padding: EdgeInsets.all(3.0),
-                          child: Text((response["privateEvent"] == "true")
+                          child: Text((event.public)
                               ? "Private Event"
                               : "Public Event",
                               style: TextStyle(
@@ -98,6 +85,30 @@ class ReusableFunctions{
                                   fontSize: 15.0
                               )
                           )
+                      ),
+                      Padding(
+                          padding: EdgeInsets.all(3.0),
+                          child: Text("Starts on " +
+                              new DateFormat.yMMMEd().format(event.date) +
+                              " " +
+                              new DateFormat.jm().format(event.date),
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15.0
+                              )),
+                      ),
+                      Padding(
+                          padding: EdgeInsets.all(3.0),
+                          child: Text("Ends on " +
+                              new DateFormat.yMMMEd().format(event.date
+                                  .add(new Duration(hours: int.parse(event.duration)))) +
+                              " " +
+                              new DateFormat.jm().format(event.date
+                                  .add(new Duration(hours: int.parse(event.duration)))),
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15.0
+                              ))
                       ),
                       Padding(
                         padding: const EdgeInsets.all(3.0),
