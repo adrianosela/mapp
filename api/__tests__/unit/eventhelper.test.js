0a121cc09e4af321b42cf533d93b135c8091ff28
Adriano <adriano.selaviles@gmail.com>
2019-11-16 16:20:37 -0800

    test getrevelant

--- api/__tests__/unit/eventhelper.test.js
+++ api/__tests__/unit/eventhelper.test.js
@@ -6,8 +6,9 @@ const Event = require("../../models/event");
 
 describe("Test Event Utils", function() {
     const mockfriends = [mongoose.Types.ObjectId(), mongoose.Types.ObjectId()];
-    let mockU, mockE;
+    let mockU, mockEWithFriends, mockENoFriends;
 
+    // create mock user and events before testing
     beforeAll(async function() {
         await mongoose.connect(process.env.MONGO_URL, {
             useNewUrlParser: true,
@@ -25,7 +26,7 @@ describe("Test Event Utils", function() {
             following: mockfriends
         })).save();
 
-        mockE = await(new Event({
+        mockEWithFriends = await(new Event({
             name: "mock event name",
             description: "mock event description",
             location: { type: "Point", coordinates: [-123.247360, 49.267941] },
@@ -36,8 +37,20 @@ describe("Test Event Utils", function() {
             followers: mockfriends
         })).save();
 
+        mockENoFriends = await(new Event({
+            name: "mock event name",
+            description: "mock event description",
+            location: { type: "Point", coordinates: [-123.247360, 49.267941] },
+            startTime: 1572928473,
+            endTime: 1572928573,
+            creator: mockU._id,
+            public: true,
+            followers: []
+        })).save();
+
         let creatorUser = await User.findById(mockU._id);
-        creatorUser.createdEvents.addToSet(mockE._id);
+        creatorUser.createdEvents.addToSet(mockEWithFriends._id);
+        creatorUser.createdEvents.addToSet(mockENoFriends._id);
         await creatorUser.save();
     });
 
@@ -52,9 +65,26 @@ describe("Test Event Utils", function() {
     });
 
     describe("Test friendsGoing", function() {
-        it("should return list of user friends going to event", function() {
-            let going = eventHelper.friendsGoing(mockU, mockE);
-            expect(going).toEqual(mockfriends.length);
+        it("should return 0 when no user friends are going to event", function() {
+            expect(eventHelper.friendsGoing(mockU, mockENoFriends)).toEqual(0);
+        });
+        it("should return number of friends going to event", function() {
+            expect(eventHelper.friendsGoing(mockU, mockEWithFriends))
+                .toEqual(mockfriends.length);
+        });
+    });
+
+    describe("Test relevantForUser", function() {
+        it("should return 'limit' amount of events", function() {
+            let events = [mockENoFriends, mockEWithFriends];
+            for (var lim = 0; lim < events.legnth; lim++) {
+                expect(eventHelper.relevantForUser(events, mockU, lim).length).toEqual(lim);
+            }
+        });
+        it("should return the most relevant event", function() {
+            let events = [mockENoFriends, mockEWithFriends];
+            expect(eventHelper.relevantForUser(events, mockU, 1).length).toEqual(1);
+            expect(eventHelper.relevantForUser(events, mockU, 1)).toContain(mockEWithFriends);
         });
     });
 });
