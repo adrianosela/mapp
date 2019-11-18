8ec152828cb1052bc5da1955912af5c4b2478fde
Adriano <adriano.selaviles@gmail.com>
2019-11-18 12:32:40 -0800

    test notifications

---
+++ api/__tests__/unit/notifications.test.js
@@ -0,0 +1,27 @@
+let notifications = require("../../notifications/notifications");
+
+describe("Test Notifications Controller", function() {
+    describe("Positive: Successful Initialization", function() {
+        it("should be disabled when given allowNotify = false", function() {
+            notifications.initialize({}, false);
+            expect(notifications.isEnabled()).toBe(false);
+        });
+    });
+    describe("Positive: Successful Notify", function() {
+        it("should not notify anyone when null array is given", function() {
+            let mockTokens;
+            notifications.initialize({}, false);
+            expect(notifications.notify("mock message", mockTokens)).toBe(0);
+        });
+        it("should not notify anyone when empty array is given", function() {
+            let mockTokens = [];
+            notifications.initialize({}, false);
+            expect(notifications.notify("mock message", mockTokens)).toBe(0);
+        });
+        it("should notify user when one user is given", function() {
+            let mockTokens = ["mock-user-token"];
+            notifications.initialize({}, false);
+            expect(notifications.notify("mock message", mockTokens)).toBe(mockTokens.length);
+        });
+    });
+});
