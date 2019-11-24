6ee89ce2b11b26a07ba7dd09db9d0d8882f98dea
Adriano Sela Aviles <adriano.selaviles@gmail.com>
2019-11-24 10:36:10 -0800

    catch empty fcm tokens (#298)

--- api/reminders/reminders.js
+++ api/reminders/reminders.js
@@ -8,7 +8,7 @@ const ONE_HOUR_IN_S = 3600;
 let remind = async function() {
     try {
         const now = Math.floor(Date.now() / 1000);
-        
+
         let events = await Event.find({})
             .gte("startTime", now)
             .lte("startTime", now + ONE_HOUR_IN_S);
@@ -20,7 +20,12 @@ let remind = async function() {
 
             let subscribedUsersTokens = [];
             for (let user of userSettings) {
-                subscribedUsersTokens.push(user.fcmToken);
+                if (user.fcmToken) {
+                    subscribedUsersTokens.push(user.fcmToken);
+                }
+                else {
+                    logger.info(`could not remind ${user.email} of event ${event._id}. no fcm token for user found`);
+                }
             }
 
             let notification = {
