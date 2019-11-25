e532159b0a0aae05671896c1d210f8e91b9966fb
Adriano Sela Aviles <adriano.selaviles@gmail.com>
2019-11-24 20:25:35 -0800

    better way of scheduling (#304)

--- api/index.js
+++ api/index.js
@@ -4,16 +4,17 @@ const app = require("./server");
 const db = require("./store/datastore");
 const notifications = require("./notifications/notifications");
 const reminder = require("./reminders/reminders");
-
-const ONE_HOUR_IN_MS = 3600000;
+const cron = require("node-cron");
 
 db.initialize(config.get("database.url"), config.get("database.name"));
 
 notifications.initialize(config.get("notifications.firebase"));
 
-setInterval(() => {
+// run a cron job in the background which sends 
+// reminder notifications at minute 30 of every hour
+cron.schedule("30 * * * *", function() {
     reminder.remindUsers();
-}, ONE_HOUR_IN_MS);
+});
 
 // serve http - when running on Google App Engine, the PORT env variable
 // gets set by the runtime. Otherwise we use the default 'port' in config
